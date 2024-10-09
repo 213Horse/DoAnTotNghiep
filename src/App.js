cb76f326367878eff25ca29a88da9302954a9047
213Horse <minhquando59@gmail.com>
2024-10-09 15:45:40 +0700

    Create source code UI

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import TripPlanner from './components/TripPlanner';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <TripPlanner />
+    </div>
+  );
+}
+
+export default App;
